e4b5408865bbe24aa13aafa82a60166c5b29eb3d
young-kim <ceo.youngkim@gmail.com>
2024-02-29 14:12:15 +0900

    feat: Add websocket-client module

---
+++ websocket-client/src/test/kotlin/io/github/youngkim90/websocketclient/WebsocketClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.youngkim90.websocketclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsocketClientApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
